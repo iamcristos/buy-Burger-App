aba9c24cfdcda3048b85187686fa0d85a170a48c
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-04-11 18:53:24 +0100

    ft(ui): add tool bar

---
+++ src/components/Toolbar/toolbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './toolbar.css'
+const toolbar = (props)=>(
+    <header className={classes.Toolbar}>
+        <div>Menu</div>
+        <div>LOGO</div>
+        <nav>...</nav>
+    </header>
+)
+
+export default toolbar
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Aux from '../../hoc/aux'
 import classes from './layout.css'
+import Toolbar from '../Toolbar/toolbar'
 const layout = (props)=> (
     <Aux>
+        <Toolbar/>
         <div>Tool bar, Sidebar, Backdrop </div>
         <main className={classes.Container}>
             {props.children}
