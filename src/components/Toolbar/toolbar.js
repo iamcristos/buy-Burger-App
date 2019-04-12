b13c7d48f4cc1732a49d87aa4fde12a6def69698
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-04-12 08:15:18 +0100

    ft(ui):navigation bar

--- src/components/Toolbar/toolbar.js
+++ src/components/Toolbar/toolbar.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import classes from './toolbar.css'
 import Logo from './Logo/logo'
+import Navigation from '.././navigation/navigationItems/navigationItems'
 const toolbar = (props)=>(
     <header className={classes.Toolbar}>
         <div>Menu</div>
         <Logo/>
-        <nav>...</nav>
+        <nav>
+            <Navigation />
+        </nav>
     </header>
 )
 
---
+++ src/components/navigation/navigationItems/navigationItem/navigationItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './navigationItem.css'
+const navigationItem = (props)=>(
+    <li className={classes.Navigation}>
+        <a className={props.active ? classes.active : null}
+        href={props.link}>{props.children}</a>
+    </li>
+)
+
+export default navigationItem
