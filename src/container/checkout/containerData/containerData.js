5bbfe97ca746fcb7806cb663d7824d4a8a373787
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-05-20 00:02:19 +0100

    complemented dynamic form requrst

--- src/container/checkout/containerData/containerData.js
+++ src/container/checkout/containerData/containerData.js
@@ -8,47 +8,94 @@ import Input from '../../../components/form/input/input'
 class Container extends Component {
 
     state = {
-        email : '',
-        name : '',
-        address : {
-            street : "",
-            postalCode: ''
-        },
+            formData: {
+                name: {
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: 'Your Name'
+                    },
+                    value: ''
+                },
+                email: {
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'email',
+                        placeholder: 'Your Email'
+                    },
+                    value: ''
+                },
+                address: {
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: 'Your Address'
+                    },
+                    value: ''
+                }, 
+            delivaryMode: {
+                elementType: 'select',
+                elementConfig: {
+                    type: 'select',
+                    options: [
+                        {value: 'fastest', displayValue:'fastest'},
+                        {value: 'slow', displayValue:'slow'}
+                    ]
+                },
+                value:''
+            },
+            },
         loading: false
     };
 
 submitCheckoutOrder = (event)=> {
     event.preventDefault();
-    this.setState({loading:true})
+    this.setState({loading:true});
+    const formData = {};
+    for (let k in this.state.formData ) {
+        formData[k] = this.state.formData[k].value
+    }
+    // console.log(formData)
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: 'Nmeregini Vincent',
-                email: 'nmereginivincent@gmail.com',
-                address: 'Lagos Nigeria'
-            },
-            delivaryMode: 'fastest'
+            oderData : formData
         }
         axios.post('/buy.json', order)
             .then(res=> {
-                console.log(res)
                 this.setState({loading:false,})
                 this.props.history.push('/')
             })
             .catch(err=> {
                 this.setState({loading:false, })
             })
+    }
 
+onChangeHandler = (event, inputField)=>{
+    const formData = {...this.state.formData}
+    const onChangeData = {...formData[inputField]}
+    onChangeData.value = event.target.value;
+    formData[inputField] = onChangeData;
+    this.setState({formData: formData})
 }
 
     render () {
-        let form = ( <form>
-            <Input inputtype='input' type='text' name='name' placeholder='Input Your Name' />
-            <Input inputtype='input' type='email' name='email' placeholder='Input Your Mail' />
-            <Input inputtype='input' type='text' name='streel' placeholder='Input Your Street' />
-            <Input inputtype='input' type='text' name='postal' placeholder='Input Your Postal Code' />
-            <Button btnType='Success' clicked={this.submitCheckoutOrder}>ORDER</Button>
+        let formDataArray = [];
+        for (let k in this.state.formData) {
+            formDataArray.push({
+                id: k,
+                data: this.state.formData[k]
+            })
+        }
+        let form = ( <form onSubmit={this.submitCheckoutOrder}>
+            {formDataArray.map(item=>(
+                <Input key = {item.id}
+                elementType={item.data.elementType}
+                elementConfig= {item.data.elementConfig}
+                value= {item.data.value}
+                changed= {(event)=>this.onChangeHandler(event,item.id)}/>
+            ))}
+            <Button btnType='Success' >ORDER</Button>
         </form>);
         if(this.state.loading) {
             form = <Spinner />
